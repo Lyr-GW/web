1a5656047283f693d721098e4543a92268118454
lvvvv <375042084@qq.com>
2021-02-18 16:55:16 +0800

    element-plus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import App from './App.vue';
 import router from './router';
-import './plugins/element.js'
 
 const app = createApp(App)
 app.mount('#app')
 app.use(router)
+app.use(ElementPlus)
