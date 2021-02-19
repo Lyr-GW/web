9755c784d446e0eb06492e2fd11ed63c849c523b
lvvvv <375042084@qq.com>
2021-02-19 13:37:23 +0800

    date chooser

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,11 +1,70 @@
 <template>
-    <h1>Content</h1>
+  <h1>Content</h1>
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-date-picker
+        v-model="value1"
+        type="datetimerange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+      >
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">带快捷选项</span>
+      <el-date-picker
+        v-model="value2"
+        type="datetimerange"
+        :shortcuts="shortcuts"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        align="right"
+      >
+      </el-date-picker>
+    </div>
 </template>
+
 <script>
-    export default {
-       name:'Content',
-    }
+export default {
+  name: "Content",
+  data() {
+    return {
+      shortcuts: [
+        {
+          text: "最近一周",
+          value: (() => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            return [start, end];
+          })(),
+        },
+        {
+          text: "最近一个月",
+          value: (() => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            return [start, end];
+          })(),
+        },
+        {
+          text: "最近三个月",
+          value: (() => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            return [start, end];
+          })(),
+        },
+      ],
+      value1: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+      value2: "",
+    };
+  },
+};
 </script>
 <style>
-
 </style>
